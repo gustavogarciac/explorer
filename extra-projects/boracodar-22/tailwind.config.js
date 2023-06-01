400b1b865346f8a10bc676374f0a80b5c1b35eb1
gustavogarciac <132074066+gustavogarciac@users.noreply.github.com>
2023-06-01 20:04:54 -0300

    Projeto Bora codar 22

---
+++ extra-projects/boracodar-22/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Heebo': 'Heebo, sans-serif'
+    },
+  },
+  plugins: [],
+}
