bce30ad9af634ca1092be36b8a93f8bdd95841cc
gustavogarciac <132074066+gustavogarciac@users.noreply.github.com>
2023-06-22 21:17:17 -0300

    doing js exercises

---
+++ javascript-conceitos/intensivao-javascript/script.js
@@ -0,0 +1,68 @@
+// 01:
+// alert("Hello, world!")
+
+//02:
+// let numberOne = 1
+// let numberTwo = 2
+// alert(numberOne + numberTwo)
+
+//03:
+// function isNotNumber(value) {
+//   isNaN(value) ? alert("Não é um número.") : alert("É um número!")
+// }
+
+// isNotNumber(52)
+
+//04:
+// function isString(value) {
+//   typeof value == "string" ? alert("É uma string.") : alert("Não é uma string.")
+// }
+
+// isString(52)
+
+//05:
+// function isBoolean(value) {
+//   typeof value == "boolean" ? alert("É um boolean.") : alert("Não é um boolean.")
+// }
+
+// isBoolean("falsy")
+
+//06:
+// function minus(numberOne, numberTwo) {
+//   return numberOne - numberTwo
+// }
+// alert(minus(5, 10))
+
+//07:
+// function multiply(numberOne, numberTwo) {
+//   return numberOne * numberTwo
+// }
+// alert(multiply(10, 10))
+
+//08:
+// function divide(numberOne, numberTwo) {
+//   return numberOne / numberTwo;
+// }
+
+// alert(divide(5, 10))
+
+//09:
+// function isEven(number) {
+//   if (number % 2 == 0) {
+//     alert(`The number ${number} is even.`)
+//   } else {
+//     alert(`The number ${number} is odd.`)
+//   }
+// }
+
+// isEven(6)
+
+//10:
+// function isOdd(number) {
+//   if (number % 2 != 0) {
+//     alert(`The number ${number} is odd.`)
+//   } else {
+//     alert(`The number ${number} is even.`)
+//   }
+// }
+// isOdd(6)
